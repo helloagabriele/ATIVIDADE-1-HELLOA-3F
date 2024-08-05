660afb2febdb5c36b648ebe22a3e8c2155e174ea
helloagabriele <133674472+helloagabriele@users.noreply.github.com>
2024-08-05 22:53:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+ const meuNome= "helloa gabriele alves";
+const idade=17;
+const altura=1.70;
+const massa=50
+const cidadeOndeMora="São Jose"
+console.log(meuNome)
+console.log(idade)
+console.log(altura)
+console.log(massa)
+console.log(cidadeOndeMora)
+
